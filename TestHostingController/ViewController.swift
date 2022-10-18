0bc2f3553d95dda72585f35ffc681c3040d3d9b7
jake <palatable7@naver.com>
2022-10-18 10:27:32 +0900

    Initial Commit

---
+++ TestHostingController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestHostingController
+//
+//  Created by 김종권 on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
